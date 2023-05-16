fa172fae8bd9d3cdcd0dc854fcdebd051c54feea
oscar.norambuena@outlook.com <oscar.norambuena@outlook.com>
2023-05-16 14:37:44 -0400

    Reinstall antd

--- renderer/pages/_app.tsx
+++ renderer/pages/_app.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 
-import '../styles/globals.css';
+import 'antd/dist/antd.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <React.Fragment>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </Head>
+      <Component {...pageProps} />
+    </React.Fragment>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- renderer/pages/next.tsx
+++ renderer/pages/next.tsx
@@ -1,24 +1,38 @@
 import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
+import {
+  Layout,
+  Result,
+} from 'antd';
+
+const {
+  Header,
+  Content,
+} = Layout;
 
 function Next() {
   return (
     <React.Fragment>
       <Head>
-        <title>Next - Nextron (with-typescript-tailwindcss)</title>
+        <title>Next - Nextron (with-typescript-ant-design)</title>
       </Head>
-      <div className='grid grid-col-1 text-2xl w-full text-center'>
-        <img className='ml-auto mr-auto' src='/images/logo.png' />
-        <span>⚡ Nextron ⚡</span>
-      </div>
-      <div className='mt-1 w-full flex-wrap flex justify-center'>
-        <Link href='/home'>
-          <a className='btn-blue'>Go to home page</a>
-        </Link>
-      </div>
-    </React.Fragment>
-  )
-}
 
-export default Next
+      <Header>
+        <Link href="/home">
+          <a>Go to home page</a>
+        </Link>
+      </Header>
+
+      <Content style={{ padding: 48 }}>
+        <Result
+          status="success"
+          title="Nextron"
+          subTitle="with Ant Design"
+        />
+      </Content>
+    </React.Fragment>
+  );
+};
+
+export default Next;
